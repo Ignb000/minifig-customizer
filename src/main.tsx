08df4ac2d5f656ddb830d3b4275641d702050d70
Ignb000 <ignas.bilius@gmail.com>
2026-01-10 15:33:49 +0200

    various fixes and root class change

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./styles/variables.css";
-import "./styles/globals.css";
-import "./styles/buttons.css";  // ← Add this line
-import App from "./App";
-import { CustomizerProvider } from "./context/CustomizerProvider";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { CustomizerProvider } from './context/CustomizerProvider';
+import './styles/buttons.css'; // ← Add this line
+import './styles/globals.css';
+import './styles/variables.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('product-customizer')!).render(
   <React.StrictMode>
     <CustomizerProvider>
       <App />
     </CustomizerProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
